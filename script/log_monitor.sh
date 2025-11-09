b85c6779f2db79fb09e7fa262470ae47bb8aa319
Sonali Madhusmita Pattnaik <sonalipattnaik752@gmail.com>
2025-11-09 22:06:01 +0530

    Add files via upload

--- script/log_monitor.sh
+++ script/log_monitor.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# Log monitoring script
+
+log_file="/var/log/syslog"
+error_log="/mnt/e/Assignment5/log_monitors/log_errors_$(date +%F_%T).txt"
+
+echo "Scanning logs for errors..."
+grep -i "error" "$log_file" > "$error_log"
+
+if [ -s "$error_log" ]; then
+    echo "Errors found! Saved in $error_log"
+else
+    echo "No errors found in logs."
+fi
 
